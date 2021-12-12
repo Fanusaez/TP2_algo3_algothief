52975ae64ce920b97a37dc3a2b3c7ea8df9e42df
Gaston <gsabaj@fi.uba.ar>
2021-12-12 20:50:40 -0300

    Filtracion de sospechosos test funcionando (agrego clases filtrador y ordenDeArresto)

---
+++ src/main/java/edu/fiuba/algo3/modelo/Filtrador.java
@@ -0,0 +1,18 @@
+package edu.fiuba.algo3.modelo;
+
+import edu.fiuba.algo3.modelo.CosasDelincuente.Delincuente;
+
+import java.util.ArrayList;
+
+public class Filtrador {
+
+    public ArrayList<Delincuente> filtrar(ArrayList<ArrayList<String>> listaCaracteristicas,ArrayList<Delincuente> listaSospechosos){
+        ArrayList<Delincuente> listaDeCoincidintes = new ArrayList<Delincuente>();
+        for(Delincuente sospechoso: listaSospechosos){
+            if (sospechoso.cumpleLasCaracteristicas(listaCaracteristicas)){
+                listaDeCoincidintes.add(sospechoso); //asi?
+            }
+        }
+        return listaDeCoincidintes;
+    }
+}
